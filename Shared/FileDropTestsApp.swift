0c7f18fd786ae98d0e07e4b8a4a84e863b078f8d
Timm Preetz <timm@preetz.us>
2021-01-09 14:33:16 +0100

    Initial Commit

---
+++ Shared/FileDropTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileDropTestsApp.swift
+//  Shared
+//
+//  Created by Timm Preetz (LunaONE) on 09.01.21.
+//
+
+import SwiftUI
+
+@main
+struct FileDropTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
